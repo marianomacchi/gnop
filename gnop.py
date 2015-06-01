96bf5261697d59185a929ea100f389d557a2cb86
marianomacchi <mariano.macchi@null.net>
2015-06-01 19:27:13 +0200

    Refactored code in classes and functions.

--- gnop.py
+++ gnop.py
@@ -1,81 +1,90 @@
 #!/bin/python3
 
 # TODO:
-# 1) make a paddle class?
-# 2) make a ball class?
-# 3) gameplay
-# 4) use pygame fonts and text to keep scorings
-# 3) group functions based on functionality (e.g.: draw_background(), etc)
+# 1) ball class
+# 2) gameplay
+# 3) use pygame fonts and text to keep scorings
 
 import pygame
 
-# Init: display and clock
-pygame.init()
-display_width = 640
-display_height = 320
-game_display = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('gnop') # display title
-game_clock = pygame.time.Clock()
+class Colors:
+    black = (0 , 0, 0) # RGB
+    white = (255 , 255, 255)
 
-# RGB colors!
-white = (255 , 255, 255)
-black = (0 , 0, 0)
-# Paddles' length and width
-paddle_height = 25
-paddle_width = 10
+class Display:
+    width = 640
+    height = 320
+    center = width // 2
+    # paddles' starting coordinates
+    left_paddle_y = (int)(height * 0.45) # half of display, on the left
+    left_paddle_x = (int)(width * 0.20)
+    right_paddle_y = (int)(height * 0.45) # half of display, on the right
+    right_paddle_x = (int)(width * 0.80)
 
-# Paddles' starting coordinates
-lpaddle_y = (int)(display_height * 0.45) # half of display, on the left
-lpaddle_x = (int)(display_width * 0.20)
-rpaddle_y = (int)(display_height * 0.45) # half of display, on the right
-rpaddle_x = (int)(display_width * 0.80)
+class Paddle:
+    height = 25
+    width = 10
+    def __init__(self, x, y):
+        self.x = x # coordinates
+        self.y = y
+        # paddle's rectangle (used for movements and collisions)
+        self.rect = pygame.Rect(self.x, self.y, Paddle.width, Paddle.height)
 
-# display's central point
-central_point = display_width // 2
+def init():
+    pygame.init()
+    game_display = pygame.display.set_mode((Display.width, Display.height))
+    pygame.display.set_caption('gnop') # display title
+    return game_display
 
-# game display rectangle (for collisions)
-game_display_rect = game_display.get_rect()
-
-# Left and Right paddles
-lpaddle = pygame.Rect(lpaddle_x, lpaddle_y, paddle_width, paddle_height)
-rpaddle = pygame.Rect(rpaddle_x, rpaddle_y, paddle_width, paddle_height)
-
-# main loop
-quit_signal = False
-while not quit_signal:
-    for event in pygame.event.get():
-        print(event)
-        if event.type == pygame.QUIT:
-            quit_signal = True
-    pressed_keys = pygame.key.get_pressed() # list? dict? of pressed keys
-    # Left paddle movements
-    if pressed_keys[pygame.K_w]:
-        lpaddle.move_ip(0, -5) # move up
-    if pressed_keys[pygame.K_s]:
-        lpaddle.move_ip(0, 5) # move down
-    # Right paddle movements
-    if pressed_keys[pygame.K_UP]:
-        rpaddle.move_ip(0, -5) # move up
-    if pressed_keys[pygame.K_DOWN]:
-        rpaddle.move_ip(0, 5) # move down
-
-    # draw background
-    game_display.fill(black)
-    # draw central line
-    for y in range(display_height+1):
-        # draw small line segments every 15 pixels, starting from the top
+def draw_background(game_display):
+    game_display.fill(Colors.black)
+    # draw central line (line segments are drawn from the top every 15 pixels)
+    for y in range(Display.height+1):
         if y%15 == 0:
-            pygame.draw.line(game_display, white, (central_point, y),
-                            (central_point , y+5), 3)
+            pygame.draw.line(game_display, Colors.white, (Display.center, y),
+                            (Display.center, y+5), 3) # thick=3
 
-    # ensure paddles are inside the game display
-    lpaddle.clamp_ip(game_display_rect)
-    rpaddle.clamp_ip(game_display_rect)
-    # draw paddles
-    pygame.draw.rect(game_display, white, lpaddle, 0) # 0 fills the rect
-    pygame.draw.rect(game_display, white, rpaddle, 0) # 0 fills the rect
-    pygame.display.update() # updates only parts of the screen
-    game_clock.tick(60) # FPS
+def draw_gameplay(game_display, LeftPaddle, RightPaddle):
+    # draw paddles (0 fills the rectangles, 1 leaves them empty)
+    pygame.draw.rect(game_display, Colors.white, LeftPaddle, 0)
+    pygame.draw.rect(game_display, Colors.white, RightPaddle, 0)
 
-pygame.quit()
-quit()
+def main():
+    game_display = init()
+    game_display_rect = game_display.get_rect() # used for collisions
+    game_clock = pygame.time.Clock()
+    # create paddles
+    LeftPaddle = Paddle(Display.left_paddle_x, Display.left_paddle_y)
+    RightPaddle = Paddle(Display.right_paddle_x, Display.right_paddle_y)
+    # main loop
+    quit_signal = False
+    while not quit_signal:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                quit_signal = True
+        pressed_keys = pygame.key.get_pressed() # list of pressed keys
+        # Left paddle movements
+        if pressed_keys[pygame.K_w]:
+            LeftPaddle.rect.move_ip(0, -5) # move up
+        if pressed_keys[pygame.K_s]:
+            LeftPaddle.rect.move_ip(0, 5) # move down
+        # Right paddle movements
+        if pressed_keys[pygame.K_UP]:
+            RightPaddle.rect.move_ip(0, -5) # move up
+        if pressed_keys[pygame.K_DOWN]:
+            RightPaddle.rect.move_ip(0, 5) # move down
+        # ensure paddles are inside the game display
+        LeftPaddle.rect.clamp_ip(game_display_rect)
+        RightPaddle.rect.clamp_ip(game_display_rect)
+        # TODO: check ball
+        draw_background(game_display)
+        # draw the paddles and the ball
+        draw_gameplay(game_display, LeftPaddle, RightPaddle)
+        pygame.display.update() # renders the display
+        game_clock.tick(60) # FPS
+    pygame.quit()
+    quit()
+
+if __name__ == '__main__':
+    main()
